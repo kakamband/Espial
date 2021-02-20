1b8d60fbf8364fed6d58d5fde7babef74a0a6a90
AvishrantsSh <avishrants@gmail.com>
2021-02-20 17:37:40 +0530

    ID Updates

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ from threading import Thread
 from config import *
 
 def notify():
-    data = {"token": "32e027c7-df87-47fb-80a0-e1ab85df80f7"}
-    url = "http://localhost:8000/notify/"
-    # url = "https://trial-ku.herokuapp.com/notify/"
+    data = {"token": "03382f1d-0369-418c-93ba-e8eaa3d40000"}
+    # url = "http://localhost:8000/notify/"
+    url = "https://trial-ku.herokuapp.com/notify/"
     with open(FILE_PATH, 'rb') as f:
         response = requests.post(url, data = data, files={'video': f})
 
